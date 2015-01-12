aa2f3b6573e3a0283d85cc7c17d9de4ddb20a1db
Karl W. Schulz <karl.w.schulz@intel.com>
2015-01-12 08:29:59 -0800

    [FSP-152]: starting point for .spec file, updated local install utility/packaging.

--- components/perf-tools/vtune-fsp/input/install_fsp_path.sh
+++ components/perf-tools/vtune-fsp/input/install_fsp_path.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+# FSP: install from release rpms into standard FSP path
+
+version=15.1.1.380310
 release=vtune_amplifier_xe_2015_update1
-relocate_ver=vtune_amplifier_xe_2015.1.1.380310
+relocate_ver=vtune_amplifier_xe_20$version
 
-for rpm in `ls $release/rpm/*.rpm` `ls $release/CLI_Install/rpm/*.rpm`; do 
-    echo $rpm
-###     rpm -ivh --nodeps \
-### 	--relocate /opt/intel/$relocate_ver=/opt/fsp/pub/vtune/$relocate_ver $rpm
-done
+skip_arch=i486.rpm
+INSTALL=0
 
-#tar cfz intel-$release-fsp.tar.gz /opt/fsp/pub/vtune/$relocate_ver
+if [ $INSTALL -eq 1 ];then
+    for rpm in `ls $release/rpm/*.rpm` `ls $release/CLI_Install/rpm/*.rpm`; do 
+        echo $rpm | grep -q "$skip_arch$" 
+        if [ $? -eq 0 ];then
+            echo "  [ ** skipping install of $rpm **]"
+            continue
+        fi
+        echo "installing $rpm...."
+        rpm -ivh --nodeps --relocate /opt/intel/$relocate_ver=/opt/fsp/pub/vtune_amplifier/$version $rpm
+    done
+fi
+
+tar cfz intel-vtune-amplifier-fsp-$version.tar.gz /opt/fsp/pub/vtune_amplifier/$version
 
 
