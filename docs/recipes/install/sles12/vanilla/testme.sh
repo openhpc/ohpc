a3cd1284efc7cd99048e5c1321580e43c9845b56
Karl W. Schulz <karl@koomie.com>
2015-02-10 08:02:22 -0600

    example use of --repo

--- docs/recipes/install/sles12/vanilla/testme.sh
+++ docs/recipes/install/sles12/vanilla/testme.sh
@@ -4,4 +4,7 @@ export COMPUTE_HOSTS="node9, node10, node11, node12"
 
 unset ENV
 export IPMI_PASSWORD=root
+export REPO=http://fsp-obs.pdx.intel.com:82/ForestPeak:/15.05:/RC1/SLE_12_Intel/ForestPeak:15.05:RC1.repo
+
 ../../../parse_doc.pl steps.tex ../../../zeus.mapping
+#../../../parse_doc.pl --repo=$REPO steps.tex ../../../zeus.mapping
