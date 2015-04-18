26648b9dd6637274ffeed9d00b66048da019b8b8
Karl W. Schulz <karl.w.schulz@intel.com>
2015-04-17 21:55:06 -0500

    encode vc.tex as part of tarball generation

--- docs/recipes/install/gen_doc_tarball.sh
+++ docs/recipes/install/gen_doc_tarball.sh
@@ -10,25 +10,47 @@ echo "Current branch name = $BRANCHNAME"
 echo "FSP version         = $VERSION"
 
 VERSION=15.16
-TARBALL=docs-fsp-$VERSION.tar.gz
+TARBALL=docs-fsp-$VERSION.tar
 
-if [ -e $TARBALL ];then
-    rm $TARBALL
+if [ -e $TARBALL.gz ];then
+    rm $TARBALL.gz
 fi
 
 echo " "
 echo "Packaging docs distribution tarball for v$VERSION..."
 
-git archive --format=tar --prefix=docs-fsp-$VERSION/ HEAD  | gzip -n > $TARBALL
+git archive --format=tar --prefix=docs-fsp-$VERSION/ HEAD  > $TARBALL 
 
-if [ ! -s ./$TARBALL ];then
+
+ABV_HASH=`git --no-pager log -1 HEAD --pretty=format:"%h%n"`
+
+echo "Including git revision info (vc.tex)..."
+echo "--> Abbrev. git hash = $ABV_HASH"
+
+./common/vc
+
+if [ ! -s vc.tex ];then 
+    echo "Error: unable to generate vc.tex"
+    exit 1
+fi
+
+cp vc.tex sles12/vanilla/
+cp vc.tex centos7/vanilla/
+
+tar rf $TARBALL --transform "flags=r;s|^|docs-fsp-$VERSION/sles12/vanilla/|" vc.tex
+tar rf $TARBALL --transform "flags=r;s|^|docs-fsp-$VERSION/centos7/vanilla/|" vc.tex
+
+gzip -n $TARBALL
+
+if [ ! -s ./$TARBALL.gz ];then
    echo "Error generating tarball.."
    exit 1
 fi
 
-numfiles=`tar tvfz ./$TARBALL | wc -l`
-md5sum=`md5sum ./$TARBALL | awk '{print $1}'`
+numfiles=`tar tvfz ./$TARBALL.gz | wc -l`
+md5sum=`md5sum ./$TARBALL.gz | awk '{print $1}'`
 
+echo " "
 echo "--> $TARBALL generated from local git repo"
 echo "--> numfiles included = $numfiles"
 echo "--> md5sum            = $md5sum"
