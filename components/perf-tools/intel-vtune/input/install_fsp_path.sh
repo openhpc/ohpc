f03b54ec4c379b74f5ded258fda2371366236fd5
Karl W. Schulz <karl.w.schulz@intel.com>
2015-06-25 05:58:50 -0700

    [FSP-388]: updating version to 16.0.1.414512 from parallel studio update3

--- components/perf-tools/intel-vtune/input/install_fsp_path.sh
+++ components/perf-tools/intel-vtune/input/install_fsp_path.sh
@@ -2,25 +2,64 @@
 
 # FSP: install from release rpms into standard FSP path
 
-version=15.2.0.393444
+version=16.0.1.414512 
 release=vtune_amplifier_xe_2015_update2
 relocate_ver=vtune_amplifier_xe_20$version
 
+input_dir=../../../compiler-families/intel-compilers/input/update2/parallel_studio_xe_2016_beta
+
 skip_arch=i486.rpm
 INSTALL=1
+POST_UNINSTALL=1
+TARBALL=1
 
-if [ $INSTALL -eq 1 ];then
-    for rpm in `ls $release/rpm/*.rpm` `ls $release/CLI_Install/rpm/*.rpm`; do 
-        echo $rpm | grep -q "$skip_arch$" 
-        if [ $? -eq 0 ];then
-            echo "  [ ** skipping install of $rpm **]"
-            continue
-        fi
-        echo "installing $rpm...."
-        rpm -ivh --nodeps --relocate /opt/intel/$relocate_ver=/opt/fsp/pub/vtune_amplifier/$version $rpm
+match_keys='intel-vtune'
+skip_keys='i486.rpm$'
+
+installed_RPMS=""
+
+# for rpm in `ls $release/rpm/*.rpm` `ls $release/CLI_Install/rpm/*.rpm`; do 
+
+for rpm in `ls $input_dir/rpm/*.rpm`; do
+
+    name=`basename $rpm`
+
+    echo $rpm | egrep -q "$skip_keys" 
+    if [ $? -eq 0 ];then
+        continue
+    fi
+
+    echo $rpm | egrep -q "$match_keys" 
+    if [ $? -eq 0 ];then
+        echo "detected VTUNE $rpm..."
+    else
+        continue
+    fi
+
+    if [ $INSTALL -eq 1 ];then
+
+        echo "--> installing $rpm...."
+#        rpm -ivh --nodeps --relocate /opt/intel/=/opt/fsp/pub/compiler/intel $rpm || exit 1
+        rpm -ivh --nodeps --relocate /opt/intel/$relocate_ver=/opt/fsp/pub/vtune_amplifier/$version $rpm || exit 1
+        installed_RPMS="$name $installed_RPMS"
+    fi
+
+done
+
+
+if [ $TARBALL -eq 1 ];then
+    tar cfz intel-vtune-amplifier-fsp-$version.tar.gz /opt/fsp/pub/vtune_amplifier/$version
+fi
+
+if [ $POST_UNINSTALL -eq 1 ];then
+    echo " "
+    for pkg in $installed_RPMS; do 
+        localrpm=`basename --suffix=.rpm $pkg`
+        echo "[post-install] removing $localrpm...."
+#        rpm -e --nodeps $localrpm
     done
 fi
 
-tar cfz intel-vtune-amplifier-fsp-$version.tar.gz /opt/fsp/pub/vtune_amplifier/$version
+
 
 
