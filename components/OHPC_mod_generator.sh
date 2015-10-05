e7eab40c1c76dda8cfc7f9df854a456871a69394
Karl W. Schulz <karl.w.schulz@intel.com>
2015-10-05 11:15:03 -0500

    update comman-line parsing to allow more args

--- components/OHPC_mod_generator.sh
+++ components/OHPC_mod_generator.sh
@@ -4,7 +4,7 @@
 # script initializaion file.
 #-----------------------------------------------------------------------
 
-if [ $# -ne 1 ];then
+if [ $# -lt 1 ];then
     echo "Usage: $0 <shell-script-to-eval> args"
     exit 1
 fi
