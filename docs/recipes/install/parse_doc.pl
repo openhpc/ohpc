c8ae53bb7a6208909b2193b03babc54c758c31b0
John A. Westlund <john.a.westlund@intel.com>
2015-10-30 13:19:28 -0700

    re-order, clean and make style more consistent. improve error messages (#82)

--- docs/recipes/install/parse_doc.pl
+++ docs/recipes/install/parse_doc.pl
@@ -20,45 +20,45 @@ my $ci_run = 0;
 GetOptions ('repo=s' => \$repo,
 	    'ci_run' => \$ci_run);
 
-if ( $#ARGV < 0) {
-    print STDERR "Usage: parse_doc <filename>\n";
+if ( $#ARGV < 0 ) {
+print "$repo $ci_run\n";
+    print STDERR "Usage: parse_doc [--repo=<reponame>] [--ci_run] <filename>\n";
+    print STDERR "  --repo	Use a different repo for install commands\n";
+    print STDERR "  --ci_run	run additional 'CI only' commands\n";
     exit 1;
 }
 
-my $file         = shift;
-
-my $inputDir = dirname(File::Spec->rel2abs($file));
-my $basename = basename($file,".tex");
+my $file        = shift;
+my $inputDir    = dirname(File::Spec->rel2abs($file));
+my $basename    = basename($file,".tex");
 chdir $inputDir;
 
 # Determine BaseOS and define package manager commands
-
 my $Install            = "";
 my $chrootInstall      = "";
 my $groupInstall       = "";
 my $groupChrootInstall = "";
 
 # parse package command macro's from input file
-
-open(IN,"<$basename.tex")  || die __LINE__ . ": Cannot open file -> $file\n";
-while(my $line = <IN>) {
-    chomp($line);
-    if($line =~ /\\newcommand\{\\install\}\{(.+)\}/ ) {
+open( IN, "<$basename.tex" ) || die __LINE__ . ": Cannot open file -> $file\n$!";
+while( my $line = <IN> ) {
+    chomp( $line );
+    if( $line =~ /\\newcommand\{\\install\}\{(.+)\}/ ) {
 	$Install = $1;
     } 
-    elsif ($line =~ /\\newcommand\{\\chrootinstall\}\{(.+)\}/ ) {
+    elsif( $line =~ /\\newcommand\{\\chrootinstall\}\{(.+)\}/ ) {
 	$chrootInstall = $1;
     }
-    elsif ($line =~ /\\newcommand\{\\groupinstall\}\{(.+)\}/ ) {
+    elsif( $line =~ /\\newcommand\{\\groupinstall\}\{(.+)\}/ ) {
 	$groupInstall = $1;
     }
-    elsif ($line =~ /\\newcommand\{\\groupchrootinstall\}\{(.+)\}/ ) {
+    elsif( $line =~ /\\newcommand\{\\groupchrootinstall\}\{(.+)\}/ ) {
 	$groupChrootInstall = $1;
     }
 }
+close( IN );
 
 # Strip escape \ from latex macro
-
 $chrootInstall      =~ s/\\\$/\$/;
 $groupChrootInstall =~ s/\\\$/\$/;
 
@@ -67,95 +67,51 @@ $groupChrootInstall =~ s/\\\$/\$/;
 # print "groupInstall        = $groupInstall\n";
 # print "groupChrootInstall  = $groupChrootInstall\n";
 
-if ($Install eq "" || $chrootInstall eq "" || $groupInstall eq "" || $groupChrootInstall eq "") {
+if( $Install eq "" || $chrootInstall eq "" || $groupInstall eq "" || $groupChrootInstall eq "" ) {
     print "Error: package manager macros not defined\n";
     exit 1;
-}
-
-close(IN);
-
-sub check_for_section_replacement {
-    my $comment = shift;
-
-    if ($comment =~ /\\ref\{sec:(\S+)\}/) {
-	my $secname = $1;
-	my $replacementText="";
-	
-	# We can only replace section information if latex document was built - otherwise we remove the section info
-	if ( -e "$inputDir/$basename.aux" ) { 
-
-	    my $secnum=`grep {sec:$secname} $inputDir/$basename.aux  | awk -v FS="{{|})" '{print \$2}' | awk -F '}' '{print \$1}'`;
-	    
-	    chomp($secnum);
-	    
-	    if ($secnum eq "") {
-		die __LINE__ . ": Unable to query section number, verify latex build is up to date"
-	    }
-	    
-	    $replacementText="(Section $secnum)";
-	}
-
-	$comment =~ s/\\ref\{sec:(\S+)/$replacementText/g;
-    }
-
-    return($comment);
-} # end check_for_section_replacement()
-
-sub update_cmd {
-    my $cmd = shift;
-
-    $cmd =~ s/\(\*\\install\*\)/$Install/;
-    $cmd =~ s/\(\*\\chrootinstall\*\)/$chrootInstall/;
-    $cmd =~ s/\(\*\\groupinstall\*\)/$groupInstall/;
-    $cmd =~ s/\(\*\\groupchrootinstall\*\)/$groupChrootInstall/;
-
-    return($cmd);
 }
 
 # The input .tex file likely includes other input files via \input{foo}.  Do a
 # first pass through the input file and accumulate a temp file which includes
 # an aggregate copy of all the tex commands.
 
-(my $fh_raw, my $tmpfile_raw ) = tempfile();
+( my $TMPFH_AGGR_STEPS, my $tmpfile_aggr_steps ) = tempfile();
+open( IN, "<$basename.tex" ) || die __LINE__ . ": Cannot open file -> $file\n$!";
 
-open(IN,"<$basename.tex")  || die __LINE__ . ": Cannot open file -> $file\n";
-
-while(my $line = <IN>) {
-
+while( my $line = <IN> ) {
     # Check for include 
     if( $line =~ /\\input\{(\S+)\}/ ) {
-
 	my $inputFile = $1;
-	# verify input file has .tex extension or add it
 
-	if($inputFile !~ /(\S+).tex/ ) {
+	# verify input file has .tex extension or add it
+	if( $inputFile !~ /(\S+).tex/ ) {
 	    $inputFile = $inputFile . ".tex";
 	}
 
-	open(IN2,"<$inputFile") || die __LINE__ . ": Cannot open embedded input file $inputFile for parsing";
+	open( IN2,"<$inputFile" ) || die __LINE__ . ": Cannot open embedded input file $inputFile for parsing\n$!";
 
-	while(my $line_embed = <IN2>) {
+	while( my $line_embed = <IN2> ) {
 	    if( $line_embed =~ /\\input\{\S+\}/ ) {
 		print "Error: nested \\input{} file parsing not supported\n";
 		exit 1;
-	    } elsif ($line !~ /^%/) {
-		print $fh_raw $line_embed;
+	    } elsif( $line !~ /^%/ ) {
+		print $TMPFH_AGGR_STEPS $line_embed;
 	    }
 	}		
-	close(IN2);
+	close( IN2 );
     } else {
-	print $fh_raw $line;
+	print $TMPFH_AGGR_STEPS $line;
     }
 }
 
-close(IN);
-close($fh_raw);
+close( IN );
+close( $TMPFH_AGGR_STEPS );
 
 # Next, parse the raw file and look for commands to execute based on delimiter
+( my $fh,my $tmpfile ) = tempfile();
 
-(my $fh,my $tmpfile) = tempfile();
-
-open(IN,"<$tmpfile_raw")  || die __LINE__ . ": Cannot open file -> $file\n";
+open( IN, "<$tmpfile_aggr_steps" ) || die __LINE__ . ": Cannot open file -> $file\n$!";
 
 my $begin_delim   = "% begin_ohpc_run";
 my $end_delim     = "% end_ohpc_run";
@@ -165,39 +121,37 @@ my $indent        = 0;
 
 print $fh "#!/bin/bash\n";
 
-while(<IN>) {
+while( <IN> ) {
     if( $_ =~ /$disable_delim/ ) {
 	next;
     }
-    if(/$begin_delim/../$end_delim/) {
-	if ($_ =~ /% ohpc_validation_newline/) {
+    if( /$begin_delim/../$end_delim/ ) {
+	if( $_ =~ /% ohpc_validation_newline/ ) {
 	    print $fh "\n";
-	} elsif ($_ =~ /% ohpc_ci_comment (.+)/) {
-	    if ( !$ci_run) {next;}
+	} elsif( $_ =~ /% ohpc_ci_comment (.+)/ ) {
+	    if ( !$ci_run ) { next; }
 	    print $fh "# $1 (CI only)\n";
-	} elsif ($_ =~ /% ohpc_comment_header (.+)/) {
-
-	    my $comment = check_for_section_replacement($1);
+	} elsif ( $_ =~ /% ohpc_comment_header (.+)/ ) {
+	    my $comment = check_for_section_replacement( $1 );
 	    my $strlen  = length $comment;
 
 	    printf $fh "\n";
 	    printf $fh "# %s\n", '-' x $strlen;
 	    print  $fh "# $comment\n";
 	    printf $fh "# %s\n", '-' x $strlen;
-	    
-	} elsif ($_ =~ /% ohpc_validation_comment (.+)/) {
-	    my $comment = check_for_section_replacement($1);
+	} elsif( $_ =~ /% ohpc_validation_comment (.+)/ ) {
+	    my $comment = check_for_section_replacement( $1 );
 	    print $fh ' ' x $indent . "# $comment\n";
-	} elsif ($_ =~ /% ohpc_indent (\d+)/) {
+	} elsif( $_ =~ /% ohpc_indent (\d+)/ ) {
 	    $indent = $1;
-	} elsif ($_ =~ /% ohpc_command (.+)/) {
-	    my $cmd = update_cmd($1);
+	} elsif($_ =~ /% ohpc_command (.+)/ ) {
+	    my $cmd = update_cmd( $1 );
 	    print $fh ' ' x $indent . "$cmd\n";
-	} elsif ($_ =~ /\[master\]\(\*\\\#\*\) (.+ <<([^ ]+).*)$/) {
+	} elsif( $_ =~ /\[master\]\(\*\\\#\*\) (.+ <<([^ ]+).*)$/ ) {
 	    my $cmd  = update_cmd($1);
 	    my $here = $2;
 
-	    if($_ =~ /^%/ && !$ci_run ) { next; } # commands that begin with a % are for CI only
+	    if( $_ =~ /^%/ && !$ci_run ) { next; } # commands that begin with a % are for CI only
 
             print $fh ' ' x $indent . "$cmd\n";
 	    my $next_line;
@@ -209,11 +163,10 @@ while(<IN>) {
 	        print $fh "$next_line\n";
 	     } while( $next_line !~ /^$here/ );
 
-	} elsif ($_ =~ /\[master\]\(\*\\\#\*\) (.+) \\$/) {
+	} elsif( $_ =~ /\[master\]\(\*\\\#\*\) (.+) \\$/ ) {
+	    my $cmd = update_cmd( $1 );
 
-	    my $cmd = update_cmd($1);
-
-	    if($_ =~ /^%/ && !$ci_run ) { next; } # commands that begin with a % are for CI only
+	    if( $_ =~ /^%/ && !$ci_run ) { next; } # commands that begin with a % are for CI only
 
 	    print $fh ' ' x $indent . "$cmd";
 	    my $next_line = <IN>;
@@ -226,12 +179,10 @@ while(<IN>) {
 	    # TODO - add loop to accomodate multi-line continuation
 	} elsif ($_ =~ /\[master\]\(\*\\\#\*\) (.+[ ]*;[ ]*do)$/) {
 	    # special treatment for do loops
-
 	    my $cmd = update_cmd($1);
 	    
-#	    print $fh "$cmd\n";
 	    print $fh ' ' x $indent . "$cmd\n";
-	    my $next_line;# = <IN>;
+	    my $next_line;
 
 	    while ( $next_line = <IN> ) {
 		last if $next_line =~ m/\s+done/;
@@ -239,7 +190,6 @@ while(<IN>) {
 		# trim leading and trailing space
 		$next_line =~ s/^\s+|\s+$//g;
 
-#		printf $fh "   %s\n",$next_line;
 		printf $fh ' ' x $indent . "   %s\n",$next_line;
 	    }
 
@@ -247,27 +197,67 @@ while(<IN>) {
 	    $next_line =~ s/^\s+|\s+$//g;
 
 	    print $fh ' ' x $indent . "$next_line\n";
-	} elsif ($_ =~ /\[master\]\(\*\\\#\*\) (.+)$/) {
+	} elsif( $_ =~ /\[master\]\(\*\\\#\*\) (.+)$/ ) {
 	    my $cmd = update_cmd($1);
 
-	    if($_ =~ /^%/ && !$ci_run ) { next; } # commands that begin with a % are for CI only
+	    if( $_ =~ /^%/ && !$ci_run ) { next; } # commands that begin with a % are for CI only
 
 	    print $fh ' ' x $indent . "$cmd\n";
-	} elsif ($_ =~ /\[postgres\]\$ (.+)$/) {
-	    my $cmd = update_cmd($1);
+	} elsif( $_ =~ /\[postgres\]\$ (.+)$/ ) {
+	    my $cmd = update_cmd( $1 );
 	    print $fh ' ' x $indent . "$cmd\n";
 	}
 
     }
 }
 
-close(IN);
-close($fh);
+close( IN );
+close( $fh );
 
 # Echo commands
-open(IN,"<$tmpfile")  || die __LINE__ . ": Cannot open file -> $tmpfile\n";
+open( IN, "<$tmpfile" ) || die __LINE__ . ": Cannot open file -> $tmpfile\n$!";
 while ( <IN> ) {
     print;
 }
 
-unlink($tmpfile) || die("Unable to remove $tmpfile");
+unlink( $tmpfile ) || die "Unable to remove $tmpfile\n$!";
+
+
+
+
+sub check_for_section_replacement {
+    my $comment = shift;
+
+    if( $comment =~ /\\ref\{sec:(\S+)\}/ ) {
+	my $secname = $1;
+	my $replacementText = "";
+	
+	# We can only replace section information if latex document was built - otherwise we remove the section info
+	if( -e "$inputDir/$basename.aux" ) { 
+	    my $secnum=`grep {sec:$secname} $inputDir/$basename.aux  | awk -v FS="{{|})" '{print \$2}' | awk -F '}' '{print \$1}'`;
+	    
+	    chomp( $secnum );
+	    
+	    if( $secnum eq "" ) {
+		die __LINE__ . ": Unable to query section number, verify latex build is up to date"
+	    }
+	    
+	    $replacementText = "(Section $secnum)";
+	}
+
+	$comment =~ s/\\ref\{sec:(\S+)/$replacementText/g;
+    }
+
+    return( $comment );
+} # end check_for_section_replacement()
+
+sub update_cmd {
+    my $cmd = shift;
+
+    $cmd =~ s/\(\*\\install\*\)/$Install/;
+    $cmd =~ s/\(\*\\chrootinstall\*\)/$chrootInstall/;
+    $cmd =~ s/\(\*\\groupinstall\*\)/$groupInstall/;
+    $cmd =~ s/\(\*\\groupchrootinstall\*\)/$groupChrootInstall/;
+
+    return( $cmd );
+}
