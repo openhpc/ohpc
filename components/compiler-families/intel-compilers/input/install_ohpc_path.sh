6777c81d92f453db89bb68984ae0e4e29fce1fe0
Karl W. Schulz <karl.w.schulz@intel.com>
2016-02-22 06:13:49 -0800

    updated install script for compilers (#173)

--- components/compiler-families/intel-compilers/input/install_ohpc_path.sh
+++ components/compiler-families/intel-compilers/input/install_ohpc_path.sh
@@ -4,20 +4,27 @@
 
 delim=ohpc
 pubdir=/opt/${delim}/pub
+modscanner=../../../OHPC_mod_generator.sh
 
-version=16.0.0-109
-release_dir=parallel_studio_xe_2016_composer_edition
+version=16.0.0-109  # 1.0
+version=16.2.181  # 1.1
+
+release_dir=parallel_studio_xe_2016_composer_edition_update2
 
 #skip_keys='i486.rpm$|/intel-ipp|/intel-mkl-pgi'
 skip_keys='i486.rpm$|intel-vtune-amplifier|intel-mpi|clck_|intel-advisor|intel-inspector|intel-itac|intel-tc-|intel-ta-'
 INSTALL=1
-TAR=1
-UNINSTALL=0
-DEVEL=1
+TAR=0
+POST_UNINSTALL=0
+DEVEL=0
+
+installed_RPMS=""
 
 # Devel RPMs
 
 for rpm in `ls $release_dir/rpm/*.rpm` ; do 
+
+    name=`basename $rpm`
 
     echo $rpm | egrep -q "$skip_keys" 
     if [ $? -eq 0 ];then
@@ -46,14 +53,20 @@ for rpm in `ls $release_dir/rpm/*.rpm` ; do
     if [ $INSTALL -eq 1 ];then
         echo "--> installing $rpm...."
         rpm -ivh --nodeps --relocate /opt/intel=${pubdir}/compiler/intel $rpm
+        installed_RPMS="$name $installed_RPMS"
     fi
 
-    if [ $UNINSTALL -eq 1 ];then
-	localrpm=`basename --suffix=.rpm $rpm`
-        echo "--> uninstalling $localrpm ...."
-        rpm -e --nodeps $localrpm
-    fi
+##     if [ $UNINSTALL -eq 1 ];then
+## 	localrpm=`basename --suffix=.rpm $rpm`
+##         echo "--> uninstalling $localrpm ...."
+##         rpm -e --nodeps $localrpm
+##     fi
+
 done
+
+# generate relevant module file input
+$modscanner ${pubdir}/compiler/intel/compilers_and_libraries_20$version/linux/bin/compilervars.sh -arch intel64 -platform linux > modfile-$delim.input
+
 
 if [ $TAR -eq 1 ];then
     if [ $DEVEL -eq 1 ];then
@@ -66,4 +79,11 @@ if [ $TAR -eq 1 ];then
 
 fi
 
-
+if [ $POST_UNINSTALL -eq 1 ];then
+    echo " "
+    for pkg in $installed_RPMS; do 
+        localrpm=`basename --suffix=.rpm $pkg`
+        echo "[post-install] removing $localrpm...."
+        rpm -e --nodeps $localrpm
+    done
+fi
