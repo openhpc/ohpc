f0e466fdcf8f19ba32cc800519be8005add808e3
Karl W. Schulz <karl.w.schulz@intel.com>
2016-10-24 08:42:19 -0500

    work on doc reorg (#321)

---
+++ docs/recipes/install/warewulf/slurm/centos7.2/manifest/update_manifest.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+../../../../common/listohpc
+../../../../common/listpatterns
+../../../../common/listchanges
+
+../../../../common/build_tables.pl
+../../../../common/build_patterns.pl
+../../../../common/build_changelog.pl
