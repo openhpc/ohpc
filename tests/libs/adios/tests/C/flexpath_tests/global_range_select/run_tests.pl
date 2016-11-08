e3bb8510e7839f4aa3e79277b3dd99024f61bd76
Karl W. Schulz <karl.w.schulz@intel.com>
2016-11-08 11:58:18 -0600

    revamping adios tests for consistency (#283)

--- tests/libs/adios/tests/C/flexpath_tests/global_range_select/run_tests.pl
+++
@@ -1,8 +0,0 @@
-$mypid = fork();
-if (! $mypid) {
-	system('mpirun arrays_read &>arrays_read.out');
-} elsif (undef $mypid) {
-	die 'fork failed';
-} else {
-	system('mpirun arrays_write &>arrays_write.out');
-}
