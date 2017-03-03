1e964b3788ce03c817b2b6d519c72b2c78b1f500
Karl W. Schulz <karl.w.schulz@intel.com>
2017-03-03 16:13:51 -0600

    Removing existence and reliance on top-level TEST_ENV file. This was previoulsy a soft link to the autoconf-generate file that is created in common/TEST_ENV. Update relevant tests to access the file from the common/ directory (#63).

--- tests/perf-tools/tau/tests/get_max_threads.c
+++ tests/perf-tools/tau/tests/get_max_threads.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <omp.h>
+//#include <unistd.h>
+//#include <omp.h>
 
 int main(int argc, char ** argv)
 {
-  int max_threads = omp_get_max_threads();
+  //  int max_threads = omp_get_max_threads();
 
-  printf("%d\n", max_threads);
+  //  printf("%d\n", max_threads);
+  printf("hello world\n");
 
   return 0;
 }
