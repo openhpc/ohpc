a8d4d9d50d8b5d77b82574d9c60d23e60884404b
Karl W. Schulz <karl.w.schulz@intel.com>
2017-05-08 15:45:52 -0500

    fix typos, include ASL 2.0 header

--- components/OHPC_mod_generator.sh
+++ components/OHPC_mod_generator.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
+#
+# OpenHPC build script/utilities
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you
+# may not use this file except in compliance with the License. You may
+# obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
 #-----------------------------------------------------------------------
-# Uility to derive modules environment variable settings from shell
-# script initializaion file.
+# Utility to derive modules environment variable settings from shell
+# script initialization file.
 #-----------------------------------------------------------------------
 
 if [ $# -lt 1 ];then
