51e13708f5ac1b411f51ec51126608f54e5a2190
Karl W. Schulz <karl.w.schulz@intel.com>
2017-06-15 19:57:41 -0500

    add support to define page breaks in long tables.  auto-detect if different versions of same component are available and adjust \multirow dimensions accordingly

--- docs/recipes/install/common/build_tables.pl
+++ docs/recipes/install/common/build_tables.pl
@@ -5,7 +5,7 @@ use strict;
 
 my @ohpcCategories    = ("admin","compiler-families","dev-tools","distro-packages","io-libs","lustre","mpi-families",
                         "parallel-libs","serial-libs","perf-tools","provisioning","rms", "runtimes");
-my @compiler_familes = ("gnu","intel");
+my @compiler_familes = ("gnu","gnu7","intel");
 my @mpi_families     = ("mvapich2","openmpi","impi","mpich");
 
 my @single_package_exceptions = ();
@@ -25,6 +25,13 @@ $mpi_exceptions{"python-scipy"} = 3;
 $mpi_exceptions{"fftw"} = 3;
 $mpi_exceptions{"mkl-blacs"} = 1;
 
+# Define any packages that should start a new page (hash value determines which
+# extra file to create)
+
+my %page_breaks = ();
+$page_breaks{"scorep-gnu7-impi-ohpc"} = 2;
+$page_breaks{"petsc-gnu-impi-ohpc"} = 2;
+$page_breaks{"phdf5-gnu-impi-ohpc"} = 2;
 
 my $longSummaryLine = 60;
 my $urlColor="logoblue";
@@ -40,6 +47,23 @@ my $numMPI_permute = 3;
 my $numCompiler_permute = 2;
 my $numMPI_permute = 8;
 
+sub write_table_header {
+    my $fd = shift;
+
+    # Table format/header
+
+    print $fd "\\newcolumntype{C}[1]{>{\\centering}p{#1}} \n";
+    print $fd "\\newcolumntype{L}[1]{>{\\raggedleft}p{#1}} \n";
+
+    print $fd "\\small\n";
+    print $fd "\\begin{tabularx}{\\textwidth}{L{\\firstColWidth{}}|C{\\secondColWidth{}}|X}\n";
+    print $fd "\\toprule\n";
+    print $fd "{\\bf RPM Package Name} & {\\bf Version} & {\\bf Info/URL}  \\\\ \n";
+    print $fd "\\midrule\n";
+    print $fd "\n";
+
+}
+
 foreach my $category (@ohpcCategories) {
     print "Building latex table for packages in the $category category...\n";
 
@@ -47,22 +71,14 @@ foreach my $category (@ohpcCategories) {
     open(IN,"<$filein")  || die "Cannot open file -> $filein\n";
 
     my $fileout="$category.tex";
-    open(OUT,">$fileout")  || die "Cannot open file -> $fileout\n";
+    open my $OUT, '>', $fileout || die "Cannot open file -> $fileout\n";
 
     # Table format/header
 
-    print OUT "\\newcolumntype{C}[1]{>{\\centering}p{#1}} \n";
-    print OUT "\\newcolumntype{L}[1]{>{\\raggedleft}p{#1}} \n";
+    print $OUT "\\newcolumntype{C}[1]{>{\\centering}p{#1}} \n";
+    print $OUT "\\newcolumntype{L}[1]{>{\\raggedleft}p{#1}} \n";
 
-#    print OUT "\\footnotesize\n";
-    print OUT "\\small\n";
-#    print OUT "\\begin{tabularx}{\\textwidth}{c|c|X}\n";
-#    print OUT "\\begin{tabularx}{\\textwidth}{C{4.5cm}|c|X}\n";
-    print OUT "\\begin{tabularx}{\\textwidth}{L{\\firstColWidth{}}|C{\\secondColWidth{}}|X}\n";
-    print OUT "\\toprule\n";
-    print OUT "{\\bf RPM Package Name} & {\\bf Version} & {\\bf Info/URL}  \\\\ \n";
-    print OUT "\\midrule\n";
-    print OUT "\n";
+    write_table_header($OUT);
 
     # Read in data and cache
 
@@ -159,7 +175,7 @@ foreach my $category (@ohpcCategories) {
 		
 	# latex entry
 
- 	print OUT "% <-- begin entry for $name_base\n";
+ 	print $OUT "% <-- begin entry for $name_base\n";
 	
 	if($compiler_package == 1 || $mpi_package == 1) {
 	    my $end_index = $i;
@@ -178,50 +194,98 @@ foreach my $category (@ohpcCategories) {
 	    my $delta = $end_index - $i + 1;
 	    print "number of package versions in family = $delta ($name_base)\n";
 
-            if($compiler_package) {
-                if( exists $compiler_exceptions{$name_base} ) {
-                    die "unexpected # of compiler families for exception -> $name_base" if ($delta != $compiler_exceptions{$name_base});
-                } else {
-                    die "unexpected # of compiler families for $name_base" if ( $delta != $numCompiler_permute) ;
-                }
-            }
+###             if($compiler_package) {
+###                 if( exists $compiler_exceptions{$name_base} ) {
+###                     die "unexpected # of compiler families for exception -> $name_base" if ($delta != $compiler_exceptions{$name_base});
+###                 } else {
+###                     die "unexpected # of compiler families for $name_base" if ( $delta != $numCompiler_permute) ;
+###                 }
+###             }
+### 
+###             if ($mpi_package) {
+###                 if( exists $mpi_exceptions{$name_base} ) {
+###                     die "unexpected # of mpi families for exception -> $name_base" if ($delta != $mpi_exceptions{$name_base});
+###                 } else {
+###                     die "unexpected # of mpi families for $name_base" if ( $delta != $numMPI_permute );
+###                 }
+###             }
 
-#	    die "unexpected # of mpi families for $name_base" if ($mpi_package && ($delta != 6) && );
-            if ($mpi_package) {
-                if( exists $mpi_exceptions{$name_base} ) {
-                    die "unexpected # of mpi families for exception -> $name_base" if ($delta != $mpi_exceptions{$name_base});
-                } else {
-                    die "unexpected # of mpi families for $name_base" if ( $delta != $numMPI_permute );
-                }
-            }
+	    # Check if all versions are equal, compiler/mpi variant additions
+	    # might introduce asymmetry that needs to be handled 
 
+	    my $versions_equal=1;
+	    my $startVer=$versionData[$i];
+	    my @verIndex = ();
+	    for my $kk ($i+1 .. $end_index) {
+		if ($versionData[$kk] ne $startVer ) {
+		    print "versions do not match for $nameData[$kk] -> $startVer,$versionData[$kk]\n";
+		    $versions_equal = 0;
+		    push(@verIndex,$kk); # index at end of current version
+		    $startVer = $versionData[$kk];
+		}
+	    }
+
+	    if(!$versions_equal) {
+		push(@verIndex,$end_index);
+	    }
 
 	    my $k = $i;
 	    for my $k ($i .. $end_index) {
 
+		# setup write to new file if page break package specified
+		if( exists $page_breaks{$nameData[$k]} ) {
+		    print $OUT "\\bottomrule\n";
+		    print $OUT "\\end{tabularx}\n";
+		    close($OUT);
+		    my $filenew = "$category" . "$page_breaks{$nameData[$k]}" . ".tex";
+		    open $OUT, '>', $filenew || die "Cannot open file -> $filenew\n";
+		    write_table_header($OUT)
+		}
+
                 my $lname = $nameData[$k];
                 $lname =~ s/_/\\_/g;
 
-		print OUT "$lname & \n";
+		if(! $versions_equal && ($k == $verIndex[0]) && (@verIndex > 1) ) {
+		    print $OUT "\\cline{1-2} ";
+		}
+		
+		print $OUT "$lname &";
+		
+		if(! $versions_equal && ($k == $verIndex[0]) && (@verIndex > 1) ) {
+		    my $local_delta = $verIndex[1] - $verIndex[0] + 1;
+		    shift @verIndex;
+		    print $OUT "\\multirow{$local_delta}{*}{$versionData[$k]}";
+		}
+		print $OUT "\n";
+		    
 
 		if($k == $i) {
 		    my $sumLength = length($summaryData[$k]);
-
-		    print OUT "\\multirow{$delta}{*}{$versionData[$k]} & \n";
-		    print OUT "\\multirow{$delta}{\\linewidth}{$summaryData[$k] ";
-		    if($sumLength <= $longSummaryLine || $delta > 2) {
-			print OUT "\\newline";
+		    if($versions_equal) {
+			print $OUT "\\multirow{$delta}{*}{$versionData[$k]} & \n";
+		    } else {
+			my $local_delta = $verIndex[0] - $i;
+			print $OUT "\\multirow{$local_delta}{*}{$versionData[$k]} & \n";
 		    }
-		    print OUT " {\\color{$urlColor} \\url{$urlData[$k]}}} \\\\ \n";
+		    print $OUT "\\multirow{$delta}{\\linewidth}{$summaryData[$k] ";
+		    if($sumLength <= $longSummaryLine || $delta > 2) {
+			print $OUT "\\newline";
+		    }
+		    print $OUT " {\\color{$urlColor} \\url{$urlData[$k]}}} \\\\ \n";
 		} else {
-		    print OUT "& \\\\ \n";
+		    if($versions_equal) {
+			print $OUT "& \\\\ \n";
+		    } else {
+			print $OUT "& \\\\ ";
+			print $OUT "\n";
+		    }
 	        }
 	    }
-	    print OUT "\\hline\n";
+	    print $OUT "\\hline\n";
 	    # skip to next package
 	    $i = $end_index+1;
 	    print "skipping\n"; 
-	    print OUT "% <-- end entry for $name_base\n\n";
+	    print $OUT "% <-- end entry for $name_base\n\n";
 	    next;
 	} else {
 
@@ -231,31 +295,31 @@ foreach my $category (@ohpcCategories) {
 	    $lname =~ s/_/\\_/g;
 
 	    
- 	    print OUT "\\multirow{2}{*}{$lname} & \n";
- 	    print OUT "\\multirow{2}{*}{$versionData[$i]} & \n";
+ 	    print $OUT "\\multirow{2}{*}{$lname} & \n";
+ 	    print $OUT "\\multirow{2}{*}{$versionData[$i]} & \n";
  	    if ($urlData[$i] ne "(none)") {
- 		print OUT "$summaryData[$i] ";
+ 		print $OUT "$summaryData[$i] ";
 		if($sumLength <= $longSummaryLine) {
-		    print OUT "\\newline";
+		    print $OUT "\\newline";
 		}
- 		print OUT " { \\color{$urlColor} \\url{$urlData[$i]}} \n"
+ 		print $OUT " { \\color{$urlColor} \\url{$urlData[$i]}} \n"
  	    } else {
- 		print OUT "\\multirow{2}{*}{$summaryData[$i]} \\\\\n";
- 		print OUT "& & \n";
+ 		print $OUT "\\multirow{2}{*}{$summaryData[$i]} \\\\\n";
+ 		print $OUT "& & \n";
  	    }
  	    
- 	    print OUT "\\\\ \\hline \n";
- 	    print OUT "% <-- end entry for $name_base\n\n";
+ 	    print $OUT "\\\\ \\hline \n";
+ 	    print $OUT "% <-- end entry for $name_base\n\n";
  	}
 
 	$i++;
     }	    
     
-    print OUT "\\bottomrule\n";
-    print OUT "\\end{tabularx}\n";
-#    print OUT "\\end{tabular}\n";
+    print $OUT "\\bottomrule\n";
+    print $OUT "\\end{tabularx}\n";
+#    print $OUT "\\end{tabular}\n";
     
-    close(OUT);
+    close($OUT);
 }
 
 
