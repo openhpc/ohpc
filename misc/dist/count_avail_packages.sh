494e3dd51b81f60f20dd1f321fb042ee6171fd60
Karl W. Schulz <karl.w.schulz@intel.com>
2017-08-25 15:17:47 -0500

    add ability to query packages from Factory repo locations (useful to get package counts prior to release). Triggered by setting USE_FACTORY environment variable.

--- misc/dist/count_avail_packages.sh
+++ misc/dist/count_avail_packages.sh
@@ -29,16 +29,32 @@ echo " "
 echo "Querying available package counts: minor_ver=${minor_ver}, micro_ver=${micro_ver}"
 echo " "
 
+
+colon=""
+factory=""
+if [ -n "$USE_FACTORY" ]; then
+    echo "[Querying from Factory repositories]"
+    colon=":"
+    factory="Factory/"
+fi
+
 for os in ${oses}; do
-    repobase="http://build.openhpc.community/OpenHPC:/${minor_ver}/${os}"
+    repobase="http://build.openhpc.community/OpenHPC:/${minor_ver}${colon}/${factory}${os}"
     if [[ $micro_ver -gt 0 ]];then
-	repoupdate="http://build.openhpc.community/OpenHPC:/${minor_ver}:/Update${micro_ver}/${os}"
+	repoupdate="http://build.openhpc.community/OpenHPC:/${minor_ver}:/Update${micro_ver}${colon}/${factory}${os}"
+    fi
+
+    echo " "
+    echo "repo base = ${repobase}"
+    if [[ $micro_ver -gt 0 ]];then
+	echo "repo update = ${repoupdate}"
     fi
 
     echo "--"
     echo "OS=${os}:"
     for arch in ${arches}; do
 	echo -n "  ${arch}: "
+
 	if [[ $micro_ver -eq 0 ]];then
 	    repoquery --archlist=${arch} --repofrompath="ohpc-base,${repobase}" --repoid=ohpc-base '*' | wc -l
 	else
