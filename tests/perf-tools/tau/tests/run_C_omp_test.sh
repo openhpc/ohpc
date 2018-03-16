530766ac874a1432747be860fecd6d236d928c8e
Reese Baird <reese.baird@intel.com>
2018-03-16 08:53:32 -0700

    test-suite: fix ldpreload for tau-intel
    Signed-off-by: Reese Baird <reese.baird@intel.com>

--- tests/perf-tools/tau/tests/run_C_omp_test.sh
+++ tests/perf-tools/tau/tests/run_C_omp_test.sh
@@ -4,7 +4,11 @@ source ./common/TEST_ENV
 unset OMP_NUM_THREADS
 export OMP_NUM_THREADS=10
 if [ "x$ARCH" == "xx86_64" ];then
-    lib=callpath-param-papi-pdt-openmp-opari-profile-trace
+    if [ "x$LMOD_FAMILY_COMPILER" == "xintel" ];then
+        lib=callpath-param-papi-pdt-openmp-profile-trace
+    else
+        lib=callpath-param-papi-pdt-openmp-opari-profile-trace
+    fi
 else
     lib=callpath-param-pdt-openmp-opari-profile-trace
 fi
