c9d5367460bfdeb97fefb6be9cef5acc7563df3b
Reese Baird <reese.baird@intel.com>
2018-03-16 13:55:38 -0700

    test-suite: update ld_preload for fixed intel tau (#683)
    Signed-off-by: Reese Baird <reese.baird@intel.com>

--- tests/perf-tools/tau/tests/run_C_omp_test.sh
+++ tests/perf-tools/tau/tests/run_C_omp_test.sh
@@ -5,7 +5,7 @@ unset OMP_NUM_THREADS
 export OMP_NUM_THREADS=10
 if [ "x$ARCH" == "xx86_64" ];then
     if [ "x$LMOD_FAMILY_COMPILER" == "xintel" ];then
-        lib=callpath-param-papi-pdt-openmp-profile-trace
+        lib=callpath-param-icpc-papi-pdt-openmp-profile-trace
     else
         lib=callpath-param-papi-pdt-openmp-opari-profile-trace
     fi
