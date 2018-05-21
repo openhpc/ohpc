1612bd94507b2ed1ff712ab0302a48aea764d484
Karl W. Schulz <karl@ices.utexas.edu>
2018-05-21 08:48:14 -0500

    tests: update C++ HDF5 src to match latest interface: printError->printErrorStack()
    Signed-off-by: Karl W. Schulz <karl@ices.utexas.edu>

--- tests/libs/hdf5/test-env-variables/1_8/CXX/chunks.cpp
+++ tests/libs/hdf5/test-env-variables/1_8/CXX/chunks.cpp
@@ -229,21 +229,21 @@ int main (void)
     // catch failure caused by the H5File operations
     catch( FileIException error )
     {
-	error.printError();
+	error.printErrorStack();
 	return -1;
     }
 
     // catch failure caused by the DataSet operations
     catch( DataSetIException error )
     {
-	error.printError();
+	error.printErrorStack();
 	return -1;
     }
 
     // catch failure caused by the DataSpace operations
     catch( DataSpaceIException error )
     {
-	error.printError();
+	error.printErrorStack();
 	return -1;
     }
     return 0;
