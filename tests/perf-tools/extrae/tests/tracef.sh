53bcde2eae17a6a7f8e32807c0fc4ecee2dbd996
Karl W. Schulz <karl@ices.utexas.edu>
2018-10-31 15:41:29 -0500

    test-suite: fix extrae wrapper (#752)
    Signed-off-by: Karl W. Schulz <karl@ices.utexas.edu>

--- tests/perf-tools/extrae/tests/tracef.sh
+++ tests/perf-tools/extrae/tests/tracef.sh
@@ -5,7 +5,9 @@ source ${EXTRAE_DIR}/etc/extrae.sh
 export EXTRAE_CONFIG_FILE=./extrae.xml
 
 if [ "$LMOD_FAMILY_MPI" == "mvapich2" ];then
-    export LD_PRELOAD=${MPI_DIR}/lib/libmpi.so:${EXTRAE_HOME}/lib/libmpitracef.so # For Fortran apps
+    export LD_PRELOAD=${MPI_DIR}/lib/libmpi.so:${EXTRAE_HOME}/lib/libmpitracef.so
+else
+    export LD_PRELOAD=${EXTRAE_HOME}/lib/libmpitracef.so
 fi
 
 ## Run the desired program
