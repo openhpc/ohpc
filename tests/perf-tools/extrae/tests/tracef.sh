b28cd9aa5d00681f7a5aa56605cc7cd387747189
Karl W. Schulz <karl@ices.utexas.edu>
2018-10-31 15:25:19 -0500

    test-suite: add to LD_PRELOAD for mvapich2/extrae (#752)
    Signed-off-by: Karl W. Schulz <karl@ices.utexas.edu>

--- tests/perf-tools/extrae/tests/tracef.sh
+++ tests/perf-tools/extrae/tests/tracef.sh
@@ -3,8 +3,10 @@
 source ${EXTRAE_DIR}/etc/extrae.sh
 
 export EXTRAE_CONFIG_FILE=./extrae.xml
-#export LD_PRELOAD=${EXTRAE_HOME}/lib/libmpitrace.so # For C apps
-export LD_PRELOAD=${EXTRAE_HOME}/lib/libmpitracef.so # For Fortran apps
+
+if [ "$LMOD_FAMILY_MPI" == "mvapich2" ];then
+    export LD_PRELOAD=${MPI_DIR}/lib/libmpi.so:${EXTRAE_HOME}/lib/libmpitracef.so # For Fortran apps
+fi
 
 ## Run the desired program
 $*
