a55e3cb899a7904ee962c80a16f6fa8d5b05d0bf
Karl W. Schulz <karl@ices.utexas.edu>
2018-11-07 15:04:15 -0600

    test-suite: add small logic to extrae tests to not require papi counters on aaarch
    Signed-off-by: Karl W. Schulz <karl@ices.utexas.edu>

--- tests/perf-tools/extrae/tests/tracef.sh
+++ tests/perf-tools/extrae/tests/tracef.sh
@@ -2,7 +2,13 @@
 
 source ${EXTRAE_DIR}/etc/extrae.sh
 
-export EXTRAE_CONFIG_FILE=./extrae.xml
+arch=`uname -m`
+
+if [ "$arch" == "aarch64" ];then
+    export EXTRAE_CONFIG_FILE=./extrae-nopapi.xml
+else
+    export EXTRAE_CONFIG_FILE=./extrae.xml
+fi
 
 if [ "$LMOD_FAMILY_MPI" == "mvapich2" ];then
     export LD_PRELOAD=${MPI_DIR}/lib/libmpi.so:${EXTRAE_HOME}/lib/libmpitracef.so
@@ -10,6 +16,6 @@ else
     export LD_PRELOAD=${EXTRAE_HOME}/lib/libmpitracef.so
 fi
 
-## Run the desired program
+# Run the desired program
 $*
 
