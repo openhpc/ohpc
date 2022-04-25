498f7ba5d5ef8bcdf981b5b3d990e4509256ce39
Adrian Reber <areber@redhat.com>
2022-04-25 15:55:59 +0200

    ci: rework github actions
    This consolidates push and PR GitHub Actions into one file and removes
    the tests/ci/Dockerfile.check_spec.
    
    GitHub Actions can directly run a test inside of a container without
    manually setting it up.
    
    This also adds a second CI test to rebuild RPMs of all changed spec
    files. This way we know that the changes at least do not break the RPM
    build process.
    
    For MPI dependent packages the rebuild will be done using all three MPI
    variants OpenHPC currently provides.
    
    Signed-off-by: Adrian Reber <areber@redhat.com>

---
+++ tests/ci/run_build.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python3
+
+import subprocess
+import logging
+import sys
+import os
+
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+
+if len(sys.argv) != 2:
+    logging.info("SKIP. Needs a git range as parameter")
+    sys.exit(0)
+
+command = [
+    'git',
+    'diff',
+    '--diff-filter=ACMRTUXB',
+    '--name-only',
+    sys.argv[1],
+]
+
+logging.info("About to run command %s" % ' '.join(command))
+
+result = subprocess.run(command, stdout=subprocess.PIPE)
+
+if result.returncode != 0:
+    logging.info("Running git failed with %s" % result)
+    sys.exit(1)
+
+error = False
+spec_found = False
+
+
+def build_srpm_and_rpm(command, family=None):
+    logging.info("About to run command %s" % ' '.join(command))
+    result = subprocess.run(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    if result.returncode != 0:
+        logging.error("Running misc/build_srpm.sh failed with %s" % result)
+        return False
+
+    if result.stderr is not None:
+        for line in result.stderr.decode('utf-8').split('\n'):
+            if 'No compatible architectures found for build' in line:
+                logging.info("Skipping unsupported architecture RPM")
+                return True
+
+    src_rpm = ""
+    for line in result.stdout.decode('utf-8').split('\n'):
+        if line.endswith('.src.rpm'):
+            src_rpm = line
+            break
+
+    if src_rpm == "":
+        logging.error("SRPM generation failed")
+        logging.error(result.stdout.decode('utf-8'))
+        logging.error(result.stderr.decode('utf-8'))
+        return False
+
+    logging.info(src_rpm)
+
+    builddep_command = [
+        'dnf',
+        '-y',
+        'builddep',
+        src_rpm,
+    ]
+    logging.info("About to run command %s" % ' '.join(builddep_command))
+    result = subprocess.run(
+        builddep_command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    if result.returncode != 0:
+        logging.error("Running 'dnf builddep' failed with %s" % result)
+        return False
+
+    logging.info(result.stdout.decode('utf-8'))
+
+    rebuild_command = [
+        'rpmbuild',
+        '--rebuild',
+        src_rpm,
+    ]
+
+    if family is not None:
+        rebuild_command += [
+            '--define',
+            'mpi_family %s' % family,
+        ]
+
+    logging.info("About to run command %s" % ' '.join(rebuild_command))
+    result = subprocess.run(
+        rebuild_command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    if result.returncode != 0:
+        logging.info("Running 'rpmbuild --rebuild' failed with %s" % result)
+        return False
+
+    logging.info(result.stdout.decode('utf-8'))
+
+    return True
+
+
+for spec in result.stdout.decode('utf-8').split('\n'):
+    if not spec.endswith('.spec'):
+        continue
+    spec_found = True
+    logging.info("\n--> Building RPM from spec file %s" % spec)
+
+    just_spec = os.path.basename(spec)
+
+    command = [
+        'misc/get_source.sh',
+        just_spec,
+    ]
+
+    logging.info("About to run command %s" % ' '.join(command))
+    result = subprocess.run(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    if result.returncode != 0:
+        logging.error("Running misc/get_source.sh failed with %s" % result)
+        error = True
+        continue
+
+    # cache spec file contents
+    infile = open(spec)
+    contents = infile.read()
+    infile.close()
+
+    if 'ohpc_mpi_dependent' in contents:
+        families = [
+            'openmpi4',
+            'mpich',
+            'mvapich2',
+        ]
+
+        for family in families:
+            # Build SRPM
+            command = [
+                'misc/build_srpm.sh',
+                just_spec.split('.')[0],
+                family,
+            ]
+            if not build_srpm_and_rpm(command, family=family):
+                error = True
+    else:
+        # Build SRPM
+        command = [
+            'misc/build_srpm.sh',
+            just_spec.split('.')[0],
+        ]
+        if not build_srpm_and_rpm(command):
+            error = True
+
+
+if not spec_found:
+    logging.info("\nSKIP. Commit without changes to a SPEC file.")
+
+if error:
+    logging.error("\nERROR: Failure during RPM rebuild.")
+    sys.exit(1)
+
+logging.info("\nNo errors found. Exiting.")
+sys.exit(0)
