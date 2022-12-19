81bdf00de9cfc348b773cbe6d03073d7e77c9e02
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2022-12-19 16:35:05 +0200

    Pre-install openmpi4-gnu12-ohpc and mpich-gnu12-ohpc
    They are needed by rms-harness, and by all other modules.
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- tests/ci/setup_slurm_and_run_tests.sh
+++ tests/ci/setup_slurm_and_run_tests.sh
@@ -20,7 +20,7 @@ dnf -y install \
 
 # Install rebuilt packages (if any)
 # shellcheck disable=SC2046 # (we want the words to be split)
-dnf -y install prun-ohpc gnu12-compilers-ohpc $(find /home/"${USER}"/rpmbuild/RPMS/ -name "*rpm") || true
+dnf -y install prun-ohpc gnu12-compilers-ohpc openmpi4-gnu12-ohpc mpich-gnu12-ohpc $(find /home/"${USER}"/rpmbuild/RPMS/ -name "*rpm") || true
 
 # Setup slurm
 echo "127.0.0.1 node0 node1" >> /etc/hosts
