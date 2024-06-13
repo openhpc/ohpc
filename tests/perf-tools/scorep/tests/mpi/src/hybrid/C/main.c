9db5eb14f3ad552c7af80beb326266b8d9dd38d5
Jan André Reuter <j.reuter@fz-juelich.de>
2024-06-13 13:43:55 +0200

    scorep: Add tests for package
    This rather large commit adds testing to the Score-P
    OpenHPC package. The following tests will be done:
    
    - Check if module is complete and if all exec. are present
    - Try to instrument a C/C++/Fortran program
    - Check if the instrumentation worked
    - Execute the instrumented programs
    - Check if the instrumented programs created experiment directories
    - Try to inspect profile and trace
    
    Signed-off-by: Jan André Reuter <j.reuter@fz-juelich.de>

---
+++ tests/perf-tools/scorep/tests/mpi/src/hybrid/C/main.c
@@ -0,0 +1,72 @@
+#define MAXN    100000
+
+#include <mpi.h>
+#include <stdio.h>
+#include <omp.h>
+
+int main(int argc, char *argv[])
+{
+  double pi  = 0.0; /* The calculated result */
+  double my_pi;     /* The partial pi calculated by one processor */
+  int    n   = 0;   /* Number of points of integration */
+  double h;         /* Reciprocal of n, interval size */
+  double x;         /* Midpoint of each rectangle's interval */
+  double f;         /* Function to integrate */
+  double sum;       /* Area of rectangles */
+  double st, ut, time;
+
+  int    i; /* do loop index */
+
+  int nr_procs; /* Number of processors */
+  int my_rank;  /* Processor's id */
+
+  /* Initialize MPI */
+  MPI_Init(&argc, &argv);
+
+  /* Number of processes */
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+  /* Get the current processor's id */
+  MPI_Comm_size(MPI_COMM_WORLD, &nr_procs);
+
+  for(n=10;n<MAXN;n*=10) {
+
+    /* start time measurement */
+    MPI_Barrier(MPI_COMM_WORLD);
+    st = MPI_Wtime();
+
+    /* Send n to all other processors */
+    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    h   = 1.0 / n; /* Calculate interval size */
+    sum = 0.0;     /* Initialize sum */
+
+
+    /* Calculate partial sums */
+    #pragma omp parallel for private(i, x, f) reduction(+ : sum)
+    for(i = my_rank + 1; i <= n; i = i + nr_procs) {
+
+      x   = (i - 0.5) * h;
+      f   = 4.0 / (1.0 + x * x);
+      sum = sum + f;
+    }
+
+    my_pi = h * sum;
+
+    /* Reduction - all subsums are added */
+    MPI_Reduce(&my_pi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    ut = MPI_Wtime()-st;
+    MPI_Reduce(&ut, &time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+
+    /* Output - done by root processor */
+    if(my_rank == 0) {
+      printf("%02d of %02d: Value of pi is: %20.16lf, n=%9d, calculated in %10.8e s\n", my_rank, nr_procs, pi, n, time);
+    }
+  }
+
+  /* Complete MPI */
+  MPI_Finalize();
+
+  return 0;
+}
