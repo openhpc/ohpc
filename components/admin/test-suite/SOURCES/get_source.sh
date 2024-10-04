ba3b193df1c3df5d5b4b299a81fb41d6defa3068
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-10-04 23:10:34 +0300

    Add get_source.sh for admin/test-suite component
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>
    (cherry picked from commit c8c3da5e29adaa07a0a98c319b315e8fd9d50f4f)

---
+++ components/admin/test-suite/SOURCES/get_source.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -xe
+
+# Note: The current working directory is the component's SPEC folder
+OHPC_ROOT_FOLDER="../../../.."
+
+# The folder that will contain the .git/ and docs/ folders
+TESTS_OHPC_FOLDER=tests-ohpc
+
+cleanup() {
+    rm -rf ${TESTS_OHPC_FOLDER}
+}
+trap cleanup EXIT
+
+# 1. Prepare
+cleanup
+mkdir -p ${TESTS_OHPC_FOLDER}
+
+# 2. Copy the local docs/
+cp -r ${OHPC_ROOT_FOLDER}/tests ${TESTS_OHPC_FOLDER}
+
+# 3. Create tests-ohpc.tar
+tar cf ../SOURCES/tests-ohpc.tar ${TESTS_OHPC_FOLDER}
