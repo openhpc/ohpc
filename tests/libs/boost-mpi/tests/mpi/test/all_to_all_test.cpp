bb4b53063191f03e6badd2c5f18d5a2be31beb13
Adrian Reber <areber@redhat.com>
2025-04-28 10:23:35 +0000

    tests: update boost tests to version 1.88.0
    Signed-off-by: Adrian Reber <areber@redhat.com>

--- tests/libs/boost-mpi/tests/mpi/test/all_to_all_test.cpp
+++ tests/libs/boost-mpi/tests/mpi/test/all_to_all_test.cpp
@@ -4,11 +4,13 @@
 // License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
+#define BOOST_TEST_MODULE AllToAll
+
 // A test of the all_to_all() collective.
 #include <boost/mpi/collectives/all_to_all.hpp>
 #include <boost/mpi/communicator.hpp>
 #include <boost/mpi/environment.hpp>
-#include <boost/test/minimal.hpp>
+#include <boost/test/included/unit_test.hpp>
 #include <algorithm>
 #include "gps_position.hpp"
 #include <boost/serialization/string.hpp>
@@ -21,95 +23,94 @@ using boost::mpi::communicator;
 using boost::mpi::packed_skeleton_iarchive;
 using boost::mpi::packed_skeleton_oarchive;
 
-template<typename Generator>
-void
-all_to_all_test(const communicator& comm, Generator generator,
-                const char* kind)
+template <typename Generator>
+void all_to_all_test(const communicator &comm, Generator generator,
+		     const char *kind)
 {
-  typedef typename Generator::result_type value_type;
+	typedef typename Generator::result_type value_type;
 
-  using boost::mpi::all_to_all;
+	using boost::mpi::all_to_all;
 
-  std::vector<value_type> in_values;
-  for (int p = 0; p < comm.size(); ++p)
-    in_values.push_back(generator((p + 1) * (comm.rank() + 1)));
+	std::vector<value_type> in_values;
+	for (int p = 0; p < comm.size(); ++p)
+		in_values.push_back(generator((p + 1) * (comm.rank() + 1)));
 
-  if (comm.rank() == 0) {
-    std::cout << "Performing all-to-all operation on " << kind << "...";
-    std::cout.flush();
-  }
-  std::vector<value_type> out_values;
-  all_to_all(comm, in_values, out_values);
+	if (comm.rank() == 0) {
+		std::cout << "Performing all-to-all operation on " << kind
+			  << "...";
+		std::cout.flush();
+	}
+	std::vector<value_type> out_values;
+	all_to_all(comm, in_values, out_values);
 
-  for (int p = 0; p < comm.size(); ++p) {
-    BOOST_CHECK(out_values[p] == generator((p + 1) * (comm.rank() + 1)));
-  }
+	for (int p = 0; p < comm.size(); ++p) {
+		BOOST_CHECK(out_values[p] ==
+			    generator((p + 1) * (comm.rank() + 1)));
+	}
 
-  if (comm.rank() == 0) {
-    std::cout << " done." << std::endl;
-  }
+	if (comm.rank() == 0) {
+		std::cout << " done." << std::endl;
+	}
 
-  (comm.barrier)();
+	(comm.barrier)();
 }
 
 // Generates integers to test with all_to_all()
-struct int_generator
-{
-  typedef int result_type;
+struct int_generator {
+	typedef int result_type;
 
-  int operator()(int p) const { return 17 + p; }
+	int operator()(int p) const
+	{
+		return 17 + p;
+	}
 };
 
 // Generates GPS positions to test with all_to_all()
-struct gps_generator
-{
-  typedef gps_position result_type;
+struct gps_generator {
+	typedef gps_position result_type;
 
-  gps_position operator()(int p) const
-  {
-    return gps_position(39 + p, 16, 20.2799);
-  }
+	gps_position operator()(int p) const
+	{
+		return gps_position(39 + p, 16, 20.2799);
+	}
 };
 
-struct string_generator
-{
-  typedef std::string result_type;
+struct string_generator {
+	typedef std::string result_type;
 
-  std::string operator()(int p) const
-  {
-    std::string result = boost::lexical_cast<std::string>(p);
-    result += " rosebud";
-    if (p != 1) result += 's';
-    return result;
-  }
+	std::string operator()(int p) const
+	{
+		std::string result = boost::lexical_cast<std::string>(p);
+		result += " rosebud";
+		if (p != 1)
+			result += 's';
+		return result;
+	}
 };
 
-struct string_list_generator
-{
-  typedef std::list<std::string> result_type;
+struct string_list_generator {
+	typedef std::list<std::string> result_type;
 
-  std::list<std::string> operator()(int p) const
-  {
-    std::list<std::string> result;
-    for (int i = 0; i <= p; ++i) {
-      std::string value = boost::lexical_cast<std::string>(i);
-      result.push_back(value);
-    }
-    return result;
-  }
+	std::list<std::string> operator()(int p) const
+	{
+		std::list<std::string> result;
+		for (int i = 0; i <= p; ++i) {
+			std::string value = boost::lexical_cast<std::string>(i);
+			result.push_back(value);
+		}
+		return result;
+	}
 };
 
-int test_main(int argc, char* argv[])
+BOOST_AUTO_TEST_CASE(test_main)
 {
-  boost::mpi::environment env(argc, argv);
+	boost::mpi::environment env;
 
-  communicator comm;
-  all_to_all_test(comm, int_generator(), "integers");
+	communicator comm;
+	all_to_all_test(comm, int_generator(), "integers");
 #if 0
   all_to_all_test(comm, gps_generator(), "GPS positions");
 #endif
-  all_to_all_test(comm, string_generator(), "string");
-  all_to_all_test(comm, string_list_generator(), "list of strings");
-
-  return 0;
+	all_to_all_test(comm, string_generator(), "string");
+	all_to_all_test(comm, string_list_generator(), "list of strings");
 }
