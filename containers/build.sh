cc8f7c6717c69644df7fc9164fd239bebf0e642c
Timothy Middelkoop <tmiddelkoop@internet2.edu>
2025-06-12 17:10:02 -0500

    Add standalone OpenHPC 3.x container
    Signed-off-by: Timothy Middelkoop <tmiddelkoop@internet2.edu>

--- containers/build.sh
+++ containers/build.sh
@@ -14,7 +14,7 @@ $container build -t openhpc/openhpc:3 -f openhpc/Containerfile openhpc \
   --build-arg USER=$user \
   --build-arg ARCH=${arch/arm64/aarch64}
 
-for I in head node ; do
+for I in container head node ; do
   echo "=== build $I"
-  $container build -t openhpc/$I -f $I/Containerfile $I
+  $container build -t openhpc/$I -f $I/Containerfile --build-arg USER=$user $I
 done
