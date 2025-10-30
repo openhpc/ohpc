e38d8379ab4507f90d7d9714c03c7700c6c7dd07
Adrian Reber <areber@redhat.com>
2025-10-30 15:42:22 +0000

    docs: update manifests
    Signed-off-by: Adrian Reber <areber@redhat.com>

--- docs/recipes/install/common/update_manifest.sh
+++ docs/recipes/install/common/update_manifest.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-#./common/listohpc
-#./common/listpatterns
-./common/listchanges
+set -euo pipefail
+set -x
 
-./common/build_tables.pl
-./common/build_patterns.pl
-./common/build_changelog.pl
+# Check if running from a directory called "manifest"
+current_dir=$(basename "$(pwd)")
+if [ "$current_dir" != "manifest" ]; then
+    echo "Error: This script must be run from a directory called 'manifest'"
+    echo "Current directory: $current_dir"
+    exit 1
+fi
+
+echo "Running manifest update from directory: $current_dir"
+
+../common/listohpc 3.4
+../common/listchanges
+../common/build_tables.pl
+../common/build_patterns.pl
+../common/build_changelog.pl
