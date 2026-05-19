85f685261e4260df10f4f6552350ecce7328e6eb
Adrian Reber <areber@redhat.com>
2026-05-19 09:54:53 +0000

    tests: switch to ruff for Python linting
    Signed-off-by: Adrian Reber <areber@redhat.com>

--- tests/ci/check_spec.py
+++ tests/ci/check_spec.py
@@ -6,36 +6,36 @@ import sys
 
 regex_wrong = [
     # make sure there is no Source42
-    '^Source42.*$',
+    "^Source42.*$",
     # make sure OHPC_macros is not listed
-    '^Source.*OHPC_macros$',
+    "^Source.*OHPC_macros$",
     # make sure there is no %changelog
-    '^%changelog.*',
+    "^%changelog.*",
     # make sure there is no DocDir
-    '^DocDir.*',
+    "^DocDir.*",
     # make sure there is no BuildRoot
-    '^BuildRoot.*',
+    "^BuildRoot.*",
     # make sure there is no %defattr(-,root,root)
-    '^%defattr(-,root,root).*',
+    "^%defattr(-,root,root).*",
     # make sure there is no global definition of PROJ_DELIM
-    '^.*%global.*PROJ_DELIM.*$',
-    ]
+    "^.*%global.*PROJ_DELIM.*$",
+]
 
 regex_required = [
     # Group designation should include %{PROJ_NAME} delimiter and
     # known component area
-    '^Group:.*%{PROJ_NAME}/(admin|compiler-families|dev-tools|distro-packages|'
-    'io-libs|lustre|meta-package|mpi-families|parallel-libs|perf-tools|'
-    'provisioning|rms|runtimes|serial-libs)$',
+    "^Group:.*%{PROJ_NAME}/(admin|compiler-families|dev-tools|distro-packages|"
+    "io-libs|lustre|meta-package|mpi-families|parallel-libs|perf-tools|"
+    "provisioning|rms|runtimes|serial-libs)$",
     # Need a URL
-    '(^URL:.*$|Url:.*$)',
-    ]
+    "(^URL:.*$|Url:.*$)",
+]
 
 if len(sys.argv) <= 1:
     print("SKIP. Needs a list of files to check")
     sys.exit(0)
 
-regex_wrong_string = '(' + '|'.join(regex_wrong) + ')'
+regex_wrong_string = "(" + "|".join(regex_wrong) + ")"
 
 print("Checking that %s does not exist" % regex_wrong_string)
 
@@ -45,12 +45,12 @@ error = False
 spec_found = False
 
 skip_ci_specs = []
-skip_ci_specs_env = os.getenv('SKIP_CI_SPECS')
+skip_ci_specs_env = os.getenv("SKIP_CI_SPECS")
 if skip_ci_specs_env:
-    skip_ci_specs = skip_ci_specs_env.rstrip().split('\n')
+    skip_ci_specs = skip_ci_specs_env.rstrip().split("\n")
 
 for spec in sys.argv[1:]:
-    if not spec.endswith('.spec'):
+    if not spec.endswith(".spec"):
         continue
     if spec in skip_ci_specs:
         print("--> Skipping spec file %s" % spec)
@@ -64,7 +64,7 @@ for spec in sys.argv[1:]:
     infile.close()
 
     # first, verify patterns which should *not* be present
-    for line in contents.split('\n'):
+    for line in contents.split("\n"):
         if pattern.match(line):
             print("    [+] Found %s" % (line.rstrip()))
             error = True
--- tests/ci/spec_to_test_mapping.py
+++ tests/ci/spec_to_test_mapping.py
@@ -14,256 +14,137 @@ import sys
 #     'required packages for test',
 # ]
 test_map = {
-    'components/rms/slurm/SPECS/slurm.spec': [
-        'munge',
-        '',
-        'magpie-ohpc pdsh-mod-slurm-ohpc pdsh-ohpc'
+    "components/rms/slurm/SPECS/slurm.spec": [
+        "munge",
+        "",
+        "magpie-ohpc pdsh-mod-slurm-ohpc pdsh-ohpc",
     ],
-    'components/dev-tools/hwloc/SPECS/hwloc.spec': [
-        'hwloc',
-        '',
-        '',
+    "components/dev-tools/hwloc/SPECS/hwloc.spec": [
+        "hwloc",
+        "",
+        "",
     ],
-    'components/rms/magpie/SPECS/magpie.spec': [
-        'munge',
-        '',
-        'pdsh-mod-slurm-ohpc pdsh-ohpc'
+    "components/rms/magpie/SPECS/magpie.spec": [
+        "munge",
+        "",
+        "pdsh-mod-slurm-ohpc pdsh-ohpc",
     ],
-    'components/admin/pdsh/SPECS/pdsh.spec': [
-        'munge',
-        '',
-        'magpie-ohpc'
+    "components/admin/pdsh/SPECS/pdsh.spec": ["munge", "", "magpie-ohpc"],
+    "components/dev-tools/easybuild/SPECS/easybuild.spec": [
+        "easybuild",
+        "",
+        "gcc-c++",
     ],
-    'components/dev-tools/easybuild/SPECS/easybuild.spec': [
-        'easybuild',
-        '',
-        'gcc-c++',
+    "components/io-libs/adios/SPECS/adios.spec": ["adios", "", ""],
+    "components/io-libs/hdf5/SPECS/hdf5.spec": ["hdf5", "", "zlib-devel"],
+    "components/parallel-libs/ptscotch/SPECS/ptscotch.spec": [
+        "ptscotch",
+        "",
+        "zlib-devel",
     ],
-    'components/io-libs/adios/SPECS/adios.spec': [
-        'adios',
-        '',
-        ''
+    "components/serial-libs/scotch/SPECS/scotch.spec": ["scotch", "", "zlib-devel"],
+    "components/parallel-libs/fftw/SPECS/fftw.spec": ["fftw", "", ""],
+    "components/parallel-libs/hypre/SPECS/hypre.spec": ["hypre", "", ""],
+    "components/parallel-libs/mfem/SPECS/mfem.spec": ["mfem", "", ""],
+    "components/parallel-libs/mumps/SPECS/mumps.spec": ["mumps", "", ""],
+    "components/parallel-libs/opencoarrays/SPECS/opencoarrays.spec": [
+        "opencoarrays",
+        "",
+        "",
     ],
-    'components/io-libs/hdf5/SPECS/hdf5.spec': [
-        'hdf5',
-        '',
-        'zlib-devel'
+    "components/parallel-libs/petsc/SPECS/petsc.spec": ["petsc", "", ""],
+    "components/io-libs/phdf5/SPECS/hdf5.spec": ["phdf5", "", ""],
+    "components/io-libs/pnetcdf/SPECS/pnetcdf.spec": ["pnetcdf", "", ""],
+    "components/io-libs/netcdf/SPECS/netcdf.spec": ["netcdf", "", ""],
+    "components/io-libs/netcdf-cxx/SPECS/netcdf-cxx4.spec": [
+        "netcdf-all",
+        "",
+        "netcdf-fortran-gnu12-mpich-ohpc netcdf-fortran-gnu12-openmpi4-ohpc",
     ],
-    'components/parallel-libs/ptscotch/SPECS/ptscotch.spec': [
-        'ptscotch',
-        '',
-        'zlib-devel'
+    "components/io-libs/netcdf-fortran/SPECS/netcdf-fortran.spec": [
+        "netcdf-all",
+        "",
+        "netcdf-cxx-gnu12-mpich-ohpc netcdf-cxx-gnu12-openmpi4-ohpc",
     ],
-    'components/serial-libs/scotch/SPECS/scotch.spec': [
-        'scotch',
-        '',
-        'zlib-devel'
+    "components/parallel-libs/scalapack/SPECS/scalapack.spec": ["scalapack", "", ""],
+    "components/parallel-libs/slepc/SPECS/slepc.spec": ["slepc", "", ""],
+    "components/serial-libs/superlu/SPECS/superlu.spec": ["superlu", "", ""],
+    "components/parallel-libs/superlu_dist/SPECS/superlu_dist.spec": [
+        "superlu_dist",
+        "",
+        "scalapack-gnu12-openmpi4-ohpc scalapack-gnu12-mpich-ohpc",
     ],
-    'components/parallel-libs/fftw/SPECS/fftw.spec': [
-        'fftw',
-        '',
-        ''
+    "components/parallel-libs/trilinos/SPECS/trilinos.spec": ["trilinos", "", ""],
+    "components/perf-tools/extrae/SPECS/extrae.spec": ["extrae", "", ""],
+    "components/perf-tools/geopm/SPECS/geopm.spec": ["geopm", "", ""],
+    "components/perf-tools/likwid/SPECS/likwid.spec": ["likwid", "", ""],
+    "components/perf-tools/papi/SPECS/papi.spec": ["papi", "", ""],
+    "components/perf-tools/scalasca/SPECS/scalasca.spec": [
+        "scalasca",
+        "",
+        "lmod-defaults-gnu12-openmpi4-ohpc",
     ],
-    'components/parallel-libs/hypre/SPECS/hypre.spec': [
-        'hypre',
-        '',
-        ''
+    "components/perf-tools/tau/SPECS/tau.spec": ["tau", "", ""],
+    "components/mpi-families/openmpi/SPECS/openmpi.spec": ["slurm", "", ""],
+    "components/mpi-families/mpich/SPECS/mpich.spec": ["slurm", "", ""],
+    "components/dev-tools/spack/SPECS/spack.spec": ["", "spack", ""],
+    "components/admin/conman/SPECS/conman.spec": ["", "oob", ""],
+    "components/dev-tools/autoconf/SPECS/autoconf.spec": [
+        "autotools",
+        "",
+        "automake-ohpc libtool-ohpc",
     ],
-    'components/parallel-libs/mfem/SPECS/mfem.spec': [
-        'mfem',
-        '',
-        ''
+    "components/dev-tools/cmake/SPECS/cmake.spec": ["cmake", "", ""],
+    "components/parallel-libs/boost/SPECS/boost.spec": ["boost", "", ""],
+    "components/serial-libs/openblas/SPECS/openblas.spec": ["openblas", "", ""],
+    "components/serial-libs/R/SPECS/R.spec": ["R", "", ""],
+    "components/perf-tools/dimemas/SPECS/dimemas.spec": [
+        "dimemas",
+        "",
+        "lmod-defaults-gnu12-openmpi4-ohpc",
     ],
-    'components/parallel-libs/mumps/SPECS/mumps.spec': [
-        'mumps',
-        '',
-        ''
+    "components/runtimes/charliecloud/SPECS/charliecloud.spec": [
+        "charliecloud",
+        "",
+        "singularity-ce",
     ],
-    'components/parallel-libs/opencoarrays/SPECS/opencoarrays.spec': [
-        'opencoarrays',
-        '',
-        ''
-    ],
-    'components/parallel-libs/petsc/SPECS/petsc.spec': [
-        'petsc',
-        '',
-        ''
-    ],
-    'components/io-libs/phdf5/SPECS/hdf5.spec': [
-        'phdf5',
-        '',
-        ''
-    ],
-    'components/io-libs/pnetcdf/SPECS/pnetcdf.spec': [
-        'pnetcdf',
-        '',
-        ''
-    ],
-    'components/io-libs/netcdf/SPECS/netcdf.spec': [
-        'netcdf',
-        '',
-        ''
-    ],
-    'components/io-libs/netcdf-cxx/SPECS/netcdf-cxx4.spec': [
-        'netcdf-all',
-        '',
-        'netcdf-fortran-gnu12-mpich-ohpc netcdf-fortran-gnu12-openmpi4-ohpc'
-    ],
-    'components/io-libs/netcdf-fortran/SPECS/netcdf-fortran.spec': [
-        'netcdf-all',
-        '',
-        'netcdf-cxx-gnu12-mpich-ohpc netcdf-cxx-gnu12-openmpi4-ohpc'
-    ],
-    'components/parallel-libs/scalapack/SPECS/scalapack.spec': [
-        'scalapack',
-        '',
-        ''
-    ],
-    'components/parallel-libs/slepc/SPECS/slepc.spec': [
-        'slepc',
-        '',
-        ''
-    ],
-    'components/serial-libs/superlu/SPECS/superlu.spec': [
-        'superlu',
-        '',
-        ''
-    ],
-    'components/parallel-libs/superlu_dist/SPECS/superlu_dist.spec': [
-        'superlu_dist',
-        '',
-        'scalapack-gnu12-openmpi4-ohpc scalapack-gnu12-mpich-ohpc'
-    ],
-    'components/parallel-libs/trilinos/SPECS/trilinos.spec': [
-        'trilinos',
-        '',
-        ''
-    ],
-    'components/perf-tools/extrae/SPECS/extrae.spec': [
-        'extrae',
-        '',
-        ''
-    ],
-    'components/perf-tools/geopm/SPECS/geopm.spec': [
-        'geopm',
-        '',
-        ''
-    ],
-    'components/perf-tools/likwid/SPECS/likwid.spec': [
-        'likwid',
-        '',
-        ''
-    ],
-    'components/perf-tools/papi/SPECS/papi.spec': [
-        'papi',
-        '',
-        ''
-    ],
-    'components/perf-tools/scalasca/SPECS/scalasca.spec': [
-        'scalasca',
-        '',
-        'lmod-defaults-gnu12-openmpi4-ohpc'
-    ],
-    'components/perf-tools/tau/SPECS/tau.spec': [
-        'tau',
-        '',
-        ''
-    ],
-    'components/mpi-families/openmpi/SPECS/openmpi.spec': [
-        'slurm',
-        '',
-        ''
-    ],
-    'components/mpi-families/mpich/SPECS/mpich.spec': [
-        'slurm',
-        '',
-        ''
-    ],
-    'components/dev-tools/spack/SPECS/spack.spec': [
-        '',
-        'spack',
-        ''
-    ],
-    'components/admin/conman/SPECS/conman.spec': [
-        '',
-        'oob',
-        ''
-    ],
-    'components/dev-tools/autoconf/SPECS/autoconf.spec': [
-        'autotools',
-        '',
-        'automake-ohpc libtool-ohpc'
-    ],
-    'components/dev-tools/cmake/SPECS/cmake.spec': [
-        'cmake',
-        '',
-        ''
-    ],
-    'components/parallel-libs/boost/SPECS/boost.spec': [
-        'boost',
-        '',
-        ''
-    ],
-    'components/serial-libs/openblas/SPECS/openblas.spec': [
-        'openblas',
-        '',
-        ''
-    ],
-    'components/serial-libs/R/SPECS/R.spec': [
-        'R',
-        '',
-        ''
-    ],
-    'components/perf-tools/dimemas/SPECS/dimemas.spec': [
-        'dimemas',
-        '',
-        'lmod-defaults-gnu12-openmpi4-ohpc'
-    ],
-    'components/runtimes/charliecloud/SPECS/charliecloud.spec': [
-        'charliecloud',
-        '',
-        'singularity-ce'
-    ],
-    'components/admin/lmod/SPECS/lmod.spec': [
-        '',
-        'lmod',
-        ''
-    ],
+    "components/admin/lmod/SPECS/lmod.spec": ["", "lmod", ""],
 }
 
 skip_ci_specs = []
-skip_ci_specs_env = os.getenv('SKIP_CI_SPECS')
+skip_ci_specs_env = os.getenv("SKIP_CI_SPECS")
 if skip_ci_specs_env:
-    skip_ci_specs = skip_ci_specs_env.rstrip().split('\n')
+    skip_ci_specs = skip_ci_specs_env.rstrip().split("\n")
 for spec in skip_ci_specs:
     if spec in test_map:
         test_map.pop(spec)
 
 if len(sys.argv) <= 1:
-    print('TESTS=() ADMIN_TESTS=() PKGS=()')
+    print("TESTS=() ADMIN_TESTS=() PKGS=()")
     sys.exit(0)
 
-tests = ''
-admin_tests = ''
-pkgs = ''
+tests = ""
+admin_tests = ""
+pkgs = ""
 
 for i in sys.argv[1:]:
     if i in test_map.keys():
         if len(tests) > 0:
-            tests += ' '
+            tests += " "
         if len(admin_tests) > 0:
-            admin_tests += ' '
+            admin_tests += " "
         if len(pkgs) > 0:
-            pkgs += ' '
+            pkgs += " "
 
         if len(test_map[i][0]) > 0:
-            tests += f'--enable-{test_map[i][0]}'
+            tests += f"--enable-{test_map[i][0]}"
         if len(test_map[i][1]) > 0:
-            admin_tests += f'--enable-{test_map[i][1]}'
+            admin_tests += f"--enable-{test_map[i][1]}"
         pkgs += test_map[i][2]
 
 print(
-    'TESTS=(%s) ADMIN_TESTS=(%s) PKGS=(%s)' % (
+    "TESTS=(%s) ADMIN_TESTS=(%s) PKGS=(%s)"
+    % (
         tests,
         admin_tests,
         pkgs,
