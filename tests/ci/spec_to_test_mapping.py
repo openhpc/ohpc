10301d7e26a8855db6a95341d731db61a5ee76ee
Adrian Reber <areber@redhat.com>
2026-06-18 08:20:41 +0000

    ci: adapt test mapping to new RPM layout for scotch
    Signed-off-by: Adrian Reber <areber@redhat.com>

--- tests/ci/spec_to_test_mapping.py
+++ tests/ci/spec_to_test_mapping.py
@@ -42,7 +42,11 @@ test_map = {
         "",
         "zlib-devel",
     ],
-    "components/serial-libs/scotch/SPECS/scotch.spec": ["scotch ptscotch", "", "zlib-devel"],
+    "components/serial-libs/scotch/SPECS/scotch.spec": [
+        "scotch ptscotch",
+        "",
+        "zlib-devel",
+    ],
     "components/parallel-libs/fftw/SPECS/fftw.spec": ["fftw", "", ""],
     "components/parallel-libs/hypre/SPECS/hypre.spec": ["hypre", "", ""],
     "components/parallel-libs/mfem/SPECS/mfem.spec": ["mfem", "", ""],
